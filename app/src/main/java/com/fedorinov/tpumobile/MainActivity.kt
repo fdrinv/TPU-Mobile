cdaa18f80363d971f2f647051d2f929de5a9afa3
Evgeny Fedorinov <fedorinovea@gmail.com>
2023-04-29 16:33:15 +0700

    Добавил Splash Screen.

--- app/src/main/java/com/fedorinov/tpumobile/MainActivity.kt
+++ app/src/main/java/com/fedorinov/tpumobile/MainActivity.kt
@@ -1,30 +1,62 @@
 package com.fedorinov.tpumobile
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.compose.rememberNavController
-import com.fedorinov.tpumobile.ui.navigation.AppNavGraph
+import androidx.compose.ui.platform.ComposeView
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.fedorinov.tpumobile.ui.theme.TPUMobileTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        // - Устанавливаем заставку Splash-экрана
+        installSplashScreen()
         super.onCreate(savedInstanceState)
+        Thread.sleep(2000L)
+        Log.i(TAG, "Compose is initialed!")
+        // - Инициализируем Compose
         setContent {
             TPUMobileTheme {
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    Text("Compose")
+                }
                 // FIXME: Нужно создать первый стартовый экран, чтобы не вылетала ошибка регистрации маршрута.
-                AppNavGraph(
+                /*AppNavGraph(
                     startDestination = "",
                     navController = rememberNavController()
-                )
+                )*/
             }
         }
+    }
+
+    /**
+     * Инициализирует Compose в приложение.
+     */
+    private fun initialCompose(context: Context) {
+        setContentView(
+            ComposeView(context).apply {
+                setContent {
+                    TPUMobileTheme {
+                        Surface(color = MaterialTheme.colorScheme.background) {
+                            Text("Compose")
+                        }
+                        // FIXME: Нужно создать первый стартовый экран, чтобы не вылетала ошибка регистрации маршрута.
+                        /*AppNavGraph(
+                            startDestination = "",
+                            navController = rememberNavController()
+                        )*/
+                    }
+                }
+            }
+        )
+    }
+
+    companion object {
+        private val TAG = MainActivity::class.simpleName
     }
 }
